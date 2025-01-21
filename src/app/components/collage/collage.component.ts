27efe32d5712ea72b0c7c58a0e174f43ae0b8acc
Josefina-Mayans-Dev <maria.mayans@sofka.com.co>
2025-01-21 12:54:47 -0300

    angular migration v2

--- src/app/components/collage/collage.component.ts
+++ src/app/components/collage/collage.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+interface CollageItem {
+  type: "image" | "video"
+  src: string
+  alt?: string
+}
+
+interface CollageRow {
+  items: CollageItem[]
+}
 
 @Component({
   selector: 'app-collage',
@@ -7,5 +17,70 @@ import { Component } from '@angular/core';
   styleUrl: './collage.component.scss'
 })
 export class CollageComponent {
+  public title = input<string>("On a quest to craft something awesome");
+  public subtitle = input<string>("to hone my skills or just for fun");
+
+  collageRows: CollageRow[] = [
+    {
+      items: [
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/Nwjx3mEoGEPr6Z0ytJSVKttdBM.png?scale-down-to=2048",
+          alt: "collage image 1",
+        },
+        { type: "video", src: "https://framerusercontent.com/assets/xbEd5nQuu7jfqgySKEiWM6qEo.mp4" },
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/lBIrIxZdsYrBmX6E6fAnq9CE.png",
+          alt: "collage image 2",
+        },
+      ],
+    },
+    {
+      items: [
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/oAJl5eDWvbfHlPjrwIFb987kAu4.png?scale-down-to=2048",
+          alt: "collage image 3",
+        },
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/UaMHiPIlMGHsAEna9IcRYjw7t8.png",
+          alt: "collage image 4",
+        },
+        { type: "video", src: "https://framerusercontent.com/assets/PT6OnNPBZFGXR6G94bjgqLXitE.mp4" },
+      ],
+    },
+    {
+      items: [
+        { type: "video", src: "https://framerusercontent.com/assets/xOq06kbNR8ICBGBNB7orUoFaHY.mp4" },
+        { type: "video", src: "https://framerusercontent.com/assets/QfUAxk804JwLz1jyK0oKMlni7M.mp4" },
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/xFdANJx6wfkQxFqhvGeNeKKb24.png",
+          alt: "collage image 5",
+        },
+      ],
+    },
+    {
+      items: [
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/T5ht5TBTRPdFyLvGcg51zTAUQEY.png",
+          alt: "collage image 6",
+        },
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/3w2g5AFuxWTbnqNlmwEXsSqDnQ.png?scale-down-to=1024",
+          alt: "collage image 7",
+        },
+        {
+          type: "image",
+          src: "https://framerusercontent.com/images/48sF0xtesVWYCLTam7FDR3E3jUU.png?scale-down-to=1024",
+          alt: "collage image 8",
+        },
+      ],
+    },
+  ]
 
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+interface SocialLink {
+  name: string
+  url: string
+  iconSrc: string
+  ariaLabel: string
+}
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +14,39 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  public email = input<string>("hello@danielsun.space");
+
+  socialLinks: SocialLink[] = [
+    {
+      name: "Twitter",
+      url: "https://x.com/danielsun_ui",
+      iconSrc: "https://framerusercontent.com/images/8yvYtcx3cfJFOTpz7A4hI9AtiI.png",
+      ariaLabel: "Twitter",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/daniel.sun_design",
+      iconSrc: "https://framerusercontent.com/images/qyHjNFaKZK3J0IgI963dEUUJ4.png",
+      ariaLabel: "Instagram",
+    },
+    {
+      name: "Dribbble",
+      url: "https://dribbble.com/daniel_sun",
+      iconSrc: "https://framerusercontent.com/images/uMA164Althqj1b2vXygS2wyBIgQ.png",
+      ariaLabel: "Dribbble",
+    },
+    {
+      name: "Threads",
+      url: "https://www.threads.net/@daniel.sun_design",
+      iconSrc: "https://framerusercontent.com/images/23QIkTXJxWXuDvTC7jrVLxmu5cA.png",
+      ariaLabel: "Threads",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/daniel-sun-47838b209/",
+      iconSrc: "https://framerusercontent.com/images/RYuwqCxHlxrSfIOO7JZreY9YX9Q.png",
+      ariaLabel: "LinkedIn",
+    },
+  ]
 
 }
